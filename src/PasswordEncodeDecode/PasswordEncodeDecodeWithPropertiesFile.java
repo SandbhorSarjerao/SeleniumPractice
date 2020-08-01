52273924dd5451ba26e620e57caac7d2aca9c25f
sandbhorsarjerao@gmail.com <sandbhorsarjerao@gmail.com>
2020-08-01 14:00:28 +0530

    Code Updated

--- src/PasswordEncodeDecode/PasswordEncodeDecodeWithPropertiesFile.java
+++ src/PasswordEncodeDecode/PasswordEncodeDecodeWithPropertiesFile.java
@@ -1,33 +1,40 @@
 package PasswordEncodeDecode;
 
-import PasswordEncodeDecode.EncodingDecodingPassword;
-
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Properties;
 
 import org.apache.commons.codec.binary.Base64;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class PasswordEncodeDecodeWithPropertiesFile {
-	
-	public static void main(String[] args) throws IOException 
-	{
+
+	public static void main(String[] args) throws IOException {
 		FileReader reader = new FileReader("src/PasswordEncodeDecode/credentialSetting.properties");
 		Properties prop = new Properties();
 		prop.load(reader);
-		String decodePassword = utils.EncryptPassword.decryptPassword(prop.getProperty("password"));
-	}	
-	
-	 public static String decodePassword(String password) {
-	    	
-	    	// Decode the Password
-	        byte[] decodePassword = Base64.decodeBase64(password.getBytes());
-	      //  System.out.println("decodePassword: " + new String(decodePassword)); 
-	    	
-	    	return new String(decodePassword);
-	    }
+		System.out.println("Original Password: " + prop.getProperty("password"));
+		// String decodePassword =
+		// utils.EncryptPassword.decryptPassword(prop.getProperty("password"));
+		String encodePassword = encryptPassword(prop.getProperty("password"));
+		System.out.println("Encrypted Password: " + encodePassword);
+
+		String decodePassword = decryptPassword(encryptPassword(prop.getProperty("password")));
+		System.out.println("Decrypted Password: " + decodePassword);
+	}
+
+	public static String encryptPassword(String password) {
+		// Encode the Password
+		byte[] encodePassword = Base64.encodeBase64(password.getBytes());
+		// System.out.println("encodePassword: " + new String(encodePassword));
+
+		return new String(encodePassword);
+	}
+
+	public static String decryptPassword(String password) {
+		// Decode the Password
+		byte[] decodePassword = Base64.decodeBase64(password.getBytes());
+		// System.out.println("decodePassword: " + new String(decodePassword));
+
+		return new String(decodePassword);
+	}
 }
